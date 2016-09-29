34c9ab9811f7cb8686c75fe169e6cea9059483db
Teddymannen <teddy_kross@hotmail.com>
2016-09-29 12:11:04 +0200

    Updated some code
    Code is working and running. Needs styling

--- Script.js
+++ Script.js
@@ -1,8 +1,8 @@
 var apiurl, myresult, apiurl_size, selected_size, searchtext, container;
 
-function flickr_ing() {
+function image_search() {
 	searchtext = document.getElementById("mySearch").value;
-	apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=108ed65e1bb3f978ef07ca07e58754a2&text=" + searchtext + "&url_q&per_page=10&format=json&nojsoncallback=1";
+	apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=108ed65e1bb3f978ef07ca07e58754a2&text=" + searchtext + "&per_page=10&format=json&nojsoncallback=1";
 	container = document.getElementById("container");
 	selected_size = 150;
 
@@ -29,11 +29,13 @@ function flickr_ing() {
 				    		if (myresult_size.width == selected_size) {
 								// container.appendChild('<p><a href="' + myresult.url + '" target="_blank"><img src="' + myresult.source + '"/></a></p>');
 								var p = document.createElement("P");
+								p.className = "unmarked";
+								p.onclick = image_mark;
 								var a = document.createElement("A");
-								a.href = myresult_size.url;
-								a.target = "_blank";
+								a.href = "#";
 								var img = document.createElement("IMG");
 								img.src = myresult_size.source;
+								img.className = "flickr_image";
 								a.appendChild(img);
 								p.appendChild(a);
 								container.appendChild(p);
@@ -56,6 +58,56 @@ function flickr_ing() {
 		// There was a connection error of some sort
 	};
 	request.send();
+}
+
+function image_mark() {
+	if (this.className === "unmarked") {
+		this.className = "marked";
+		return false;
+	} else {
+		this.className = "unmarked";
+		return false;
+	}
+}
+
+function image_filter() {
+	var unmarked = document.getElementsByClassName("unmarked");
+	while (unmarked[0]) {
+		unmarked[0].parentNode.removeChild(unmarked[0]);
+	}
+
+	var flickr_images = document.getElementsByClassName("flickr_image");
+	for (var i = 0; i < flickr_images.length; i++) {
+		var image_source = flickr_images[i].src;
+		var flickr_without_size = image_source.slice(0, -6);
+		var flickr_size = image_source.slice(-6, 9999);
+		if (flickr_size !== "_s.jpg") {
+			flickr_images[i].src = flickr_without_size + "_s.jpg";
+		}
+	}
+
+	var flickr_gallery = document.getElementsByClassName("marked");
+	while (flickr_gallery[0]) {
+		// flickr_gallery[0].setAttribute("onclick", "image_larger()");
+		flickr_gallery[0].onclick = image_larger;
+		flickr_gallery[0].className = "gallery";
+	}
+}
+function image_larger() {
+	if (this.className === "gallery") {
+		var gallery_image = this.getElementsByClassName("flickr_image");
+		var image_source = gallery_image[0].src;
+		var flickr_without_size = image_source.slice(0, -6);
+		var flickr_size = image_source.slice(-6, 9999);
+		if (flickr_size !== "_z.jpg") {
+			gallery_image[0].src = flickr_without_size + "_z.jpg";
+			return false;
+		}
+		else {
+			gallery_image[0].src = flickr_without_size + "_s.jpg";
+			return false;
+		}
+	}
 }
 
 // $(document).ready(function(){
